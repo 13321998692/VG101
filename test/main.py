b09ec4f6e7a7dccada3860be8846d6bfb50fe9b7
King <13321998692@163.com>
2021-12-20 04:57:26 +0000

    final

--- test/main.py
+++ test/main.py
@@ -11,5 +11,35 @@ class VideoFetcher:
         pass
 
     def init(self):
+        self.todoList=[]
+        self.currentJson={}
+    
+    def _getTodo(self):
+        # Get current status
+        modelData=''
+        with open('out.txt','r',encoding='utf-8',errors='ignore') as f:
+            modelData=f.read()
+        modelData=modelData.split()
+        for item in modelData:
+            if not os.path.exists(f'data/{item}/info.json'):
+                self.todoList.append(item)
+        if not item:
+            print('Finished')
+            exit(0)
+
+    def _loadJson(self,pageNumber):
+        with open(f'data/{pageNumber}/{pageNumber}.json') as f:
+            self.currentJson=json.load(f)
+    
+    def _getAllVideoLink(self):
+        # From currentJson get data
+        playlist=self.currentJson['Playlist']
+        for index,episode in enumerate(playlist):
+            episode[0]=self._getAllVideoLink(episode[0])
+            playlist[index]=episode
         
-        self.soup=
+
+
+    def _getVideoLink(self,videoID):
+
+        
