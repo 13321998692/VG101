ff276ed7bc775bb089a54a1e7c34f4efe4fcb24d
King <13321998692@163.com>
2021-12-20 04:24:37 +0000

    test

---
+++ test/main.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+import os
+import requests
+from bs4 import BeautifulSoup
+import json
+import threading
+
+class VideoFetcher:
+    def __init__(self):
+        pass
+
+    def init(self):
+        
+        self.soup=
